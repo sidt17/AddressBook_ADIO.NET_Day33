f810c046abd078ca003f495888ae6c35ecb35916
Siddhant <santoshshrivastava39@gmail.com>
2022-04-26 16:57:14 +0530

    Updated

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -8,43 +8,43 @@ namespace AddressBookSystem
         {
 
             Console.WriteLine("Welcome to Address Book System");
-
-            AddressModelClass addressbook = new AddressModelClass();
+            Console.WriteLine("Rohit Details deleted successfully");
+            // AddressModelClass addressbook = new AddressModelClass();
             AddressBook Details = new AddressBook();
 
-            Console.WriteLine("Enter the First Name :");
-            addressbook.FirstName = Console.ReadLine();
+            //Console.WriteLine("Enter the First Name :");
+            //addressbook.FirstName = Console.ReadLine();
 
-            Console.WriteLine("Enter the Last Name :");
-            addressbook.LastName = Console.ReadLine();
+            //Console.WriteLine("Enter the Last Name :");
+            //addressbook.LastName = Console.ReadLine();
 
-            Console.WriteLine("Enter the Address :");
-            addressbook.Address = Console.ReadLine();
+            //Console.WriteLine("Enter the Address :");
+            //addressbook.Address = Console.ReadLine();
 
-            Console.WriteLine("Enter the City :");
-            addressbook.City = Console.ReadLine();
-
-
-
-            Console.WriteLine("Enter the State :");
-            addressbook.State = Console.ReadLine();
-
-            Console.WriteLine("Enter the ZipCode :");
-            addressbook.ZipCode = Convert.ToInt32(Console.ReadLine());
-
-
-            Console.WriteLine("Enter the PhoneNumber :");
-            addressbook.PhoneNumber = Convert.ToInt64(Console.ReadLine());
-
-            Console.WriteLine("Enter the EmailId :");
-            addressbook.EmailId = Console.ReadLine();
+            //Console.WriteLine("Enter the City :");
+            //addressbook.City = Console.ReadLine();
 
 
 
+            //Console.WriteLine("Enter the State :");
+            //addressbook.State = Console.ReadLine();
 
-            Details.AddDetails(addressbook);
+            //Console.WriteLine("Enter the ZipCode :");
+            //addressbook.ZipCode = Convert.ToInt32(Console.ReadLine());
 
-            addressbook.EditDetails();
+
+            //Console.WriteLine("Enter the PhoneNumber :");
+            //addressbook.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+
+            //Console.WriteLine("Enter the EmailId :");
+            //addressbook.EmailId = Console.ReadLine();
+
+
+
+
+            //Details.AddDetails(addressbook);
+
+            Details.DeleteContacts("Rohit");
         }
 
     }
