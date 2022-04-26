938b92a6a53cf2d472cdacad1aedd7e6974fb77a
Siddhant <santoshshrivastava39@gmail.com>
2022-04-26 15:34:52 +0530

    call in main method

---
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Address Book System");
+
+            AddressModelClass addressbook = new AddressModelClass();
+            AddressBook Details = new AddressBook();
+
+            Console.WriteLine("Enter the First Name :");
+            addressbook.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter the Last Name :");
+            addressbook.LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter the Address :");
+            addressbook.Address = Console.ReadLine();
+
+            Console.WriteLine("Enter the City :");
+            addressbook.City = Console.ReadLine();
+
+
+
+            Console.WriteLine("Enter the State :");
+            addressbook.State = Console.ReadLine();
+
+            Console.WriteLine("Enter the ZipCode :");
+            addressbook.ZipCode = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter the PhoneNumber :");
+            addressbook.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine("Enter the EmailId :");
+            addressbook.EmailId = Console.ReadLine();
+
+
+
+
+            Details.AddDetails(addressbook);
+
+            addressbook.EditDetails();
+        }
+
+    }
+}
+
